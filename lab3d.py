5acb0b4a9f9ba99146fbfc8841f9b0248b5dcaa2
Patrick Gerrard <pgerrard1@myseneca.ca>
2025-01-31 00:55:29 -0500

    Lab3 completed

---
+++ lab3d.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author ID: pgerrard1
+
+import subprocess
+
+def free_space():
+    result = subprocess.run("df -h | grep '/$' | awk '{print $4}'", 
+                            shell=True, 
+                            capture_output=True, 
+                            text=True)
+    return result.stdout.strip()
+
+if __name__ == '__main__':
+    print(free_space())
